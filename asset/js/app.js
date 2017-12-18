a1b9a1b0ec875699a2edf164ae422e58b7fa233c
meliveloz <mvelozcampos@gmail.com>
2017-12-17 22:15:37 -0300

    data, friends message

--- asset/js/app.js
+++ asset/js/app.js
@@ -9,104 +9,37 @@ $(document).ready(function(){
 
   });
 
-	$("#ana-maria").click(function(){
-		$(".hide-hide").empty();
-		$("#comment-here").empty();
-		$(".static").empty();
-		$(".hide-photo1").append("<img src='prueba/proyecto/img/anamaria.jpg' class='main-user-photo logo hide-hide'>");
-		$(".hide-paragraph1").append("</div class='hide-hide'><h5>Ana María</h5>");
-		$("#comment-here").append("<div class='comment-div static'><img class='photo-chat' src='prueba/proyecto/img/anamaria.jpg'><h5 class='name-user-2'>Ana Maria</h5><p>que estas haciendo?</p></div>");
+  for(i=0 ; i< friends.length ; i++){
+  	for (n=0;n<friends[i].message.length;n++){
+		$(".section-user").append("<div class='div-input-search'><img "+friends[i].photo+" class='main-user-photo'><span class='name'>"+friends[i].name+"</span><h6 class='litle-chat'>"+friends[i].message[n]+"</h6></div>");
+	
 
-	});
+	};
+  };
 
-	$("#lab-chile").click(function(){
-		$(".hide-hide").empty();
-		$("#comment-here").empty();
-		$(".static").empty();
-		$(".hide-photo1").append("<img src='prueba/proyecto/img/logocodeacademy.png' class='main-user-photo logo hide-hide'>");
-		$(".hide-paragraph1").append("</div class='hide-hide'><h5>Laboratoria Chile</h5>");
-		$("#comment-here").append("<div class='comment-div static'><img class='photo-chat' src='prueba/proyecto/img/anamaria.jpg'><h5 class='name-user-2'>Ana Maria</h5><p>y que paso?</p></div>");
-		$("#comment-here").append("<div class='comment-div static'><img class='photo-chat' src='prueba/proyecto/img/mariana.jpg'><h5 class='name-user-3'>Mariana</h5><p>con quien? con rudolf?</p></div>");
-
-	});
- 
-	$("#aldo").click(function(){
-		$(".hide-hide").empty();
-		$("#comment-here").empty();
-		$(".static").empty();
-		$(".hide-photo1").append("<img src='prueba/proyecto/img/aldo.jpg' class='main-user-photo logo hide-hide'>");
-		$(".hide-paragraph1").append("</div class='hide-hide'><h5>Aldo</h5>");
-		$("#comment-here").append("<div class='comment-div static'><img class='photo-chat' src='prueba/proyecto/img/aldo.jpg'><h5 class='name-user-2'>Aldo</h5><p>si, la mely es cool</p></div>");
-
-	});
-
-	$("#gian").click(function(){
-		$(".hide-hide").empty();
-		$("#comment-here").empty();
-		$(".static").empty();
-		$(".hide-photo1").append("<img src='prueba/proyecto/img/gian.jpg' class='main-user-photo logo hide-hide'>");
-		$(".hide-paragraph1").append("</div class='hide-hide'><h5>Gian</h5>");
-		$("#comment-here").append("<div class='comment-div static'><img class='photo-chat' src='prueba/proyecto/img/gian.jpg'><h5 class='name-user-2'>Gian</h5><p>oye. que calor</p></div>");
+  $(".div-input-search").click(function(){
+  	var nini=$(this).children("img").attr("alt");
+  	for(i=0;i< friends.length; i++){
+  		if( nini== friends[i].name){
+  			$(".hide-photo1").empty();
+  			$(".hide-paragraph1").empty();
+  			$(".comment-chat").empty();
+  			$(".hide-photo1").append("<img "+friends[i].photo+" class='main-user-photo logo'>");
+  			$(".hide-paragraph1").append("<h5>"+friends[i].name+"</h5>");
 
 
-	});
+  			for (var j = 0; j < friends[i].message.length; j++) {
+  				$(".comment-chat").append("<div class='comment-div'><img "+friends[i].photo+" class='photo-chat'><h5 class='name-user-2'>"+friends[i].name+"</h5><p>"+friends[i].message[j]+"</p></div>");
+  				
+  			};
+
+  		};
+  	};
 
 
- 	$("#jose").click(function(){
-		$(".hide-hide").empty();
-		$("#comment-here").empty();
-		$(".static").empty();
-		$(".hide-photo1").append("<img src='prueba/proyecto/img/jose.jpg' class='main-user-photo logo hide-hide'>");
-		$(".hide-paragraph1").append("</div class='hide-hide'><h5>Jose</h5>");
-		$("#comment-here").append("<div class='comment-div static'><img class='photo-chat' src='prueba/proyecto/img/Jose.jpg'><h5 class='name-user-2'>Jose</h5><p>what???</p></div>");
+  });
+   
 
-
-	});
-
- 	$("#katy").click(function(){
-		$(".hide-hide").empty();
-		$("#comment-here").empty();
-		$(".static").empty();
-		$(".hide-photo1").append("<img src='prueba/proyecto/img/katy.jpg' class='main-user-photo logo hide-hide'>");
-		$(".hide-paragraph1").append("</div class='hide-hide'><h5>Katy</h5>");
-		$("#comment-here").append("<div class='comment-div static'><img class='photo-chat' src='prueba/proyecto/img/katy.jpg'><h5 class='name-user-2'>Katy</h5><p>no creo</p></div>");
-
-
-	});
-
-	$("#mariana").click(function(){
-		$(".hide-hide").empty();
-		$("#comment-here").empty();
-		$(".static").empty();
-		$(".hide-photo1").append("<img src='prueba/proyecto/img/mariana.jpg' class='main-user-photo logo hide-hide'>");
-		$(".hide-paragraph1").append("</div class='hide-hide'><h5>Mariana</h5>");
-		$("#comment-here").append("<div class='comment-div static'><img class='photo-chat' src='prueba/proyecto/img/mariana.jpg'><h5 class='name-user-2'>Mariana</h5><p>estas?</p></div>");
-
-
-	});
-
-
-	$("#maria-paula").click(function(){
-		$(".hide-hide").empty();
-		$("#comment-here").empty();
-		$(".static").empty();
-		$(".hide-photo1").append("<img src='prueba/proyecto/img/mariapaula.jpg' class='main-user-photo logo hide-hide'>");
-		$(".hide-paragraph1").append("</div class='hide-hide'><h5>Maria Paula</h5>");
-		$("#comment-here").append("<div class='comment-div static'><img class='photo-chat' src='prueba/proyecto/img/mariapaula.jpg'><h5 class='name-user-2'>Maria Paula</h5><p>jajajajaja</p></div>");
-
-
-	});
-
-	$("#raymi").click(function(){
-		$(".hide-hide").empty();
-		$("#comment-here").empty();
-		$(".static").empty();
-		$(".hide-photo1").append("<img src='prueba/proyecto/img/raymi.jpg' class='main-user-photo logo hide-hide'>");
-		$(".hide-paragraph1").append("</div class='hide-hide'><h5>Raymi</h5>");
-		$("#comment-here").append("<div class='comment-div static'><img class='photo-chat' src='prueba/proyecto/img/raymi.jpg'><h5 class='name-user-2'>Raymi</h5><p>el reto es hasta hoy</p></div>");
-
-
-	});
-
+	
 });
 
---
+++ asset/js/data.js
@@ -0,0 +1,63 @@
+var friends = [
+  {
+    name: "Aldo",
+    message: ["hola que tal, como estas?"],
+    photo: "src='prueba/proyecto/img/aldo.jpg' alt='Aldo'"
+   
+  },
+  {
+    name: "Ana Maria",
+    message: ["jajaja, que flojera"],
+    photo: "src='prueba/proyecto/img/anamaria.jpg' alt='Ana Maria'"
+   
+  },
+  {
+    name: "Andrea",
+    message: ["estas??, rudolf?"],
+    photo: "src='prueba/proyecto/img/andrea.jpg' alt='Andrea'"
+    
+  },
+  {
+    name: "Laboratoria Chile",
+    message: ["hola que tal, como estas?"],
+    photo: "src='prueba/proyecto/img/logocodeacademy.png' alt='Laboratoria Chile'"
+    
+  },
+  {
+    name: "Gian",
+    message: ["hola que tal, como estas?"],
+    photo: "src='prueba/proyecto/img/gian.jpg' alt='Gian'"
+   
+  },
+  {
+    name: "Jose",
+    message: ["hola que tal, como estas?"],
+    photo: "src='prueba/proyecto/img/jose.jpg' alt='Jose'"
+   
+  },
+  {
+    name: "Katy",
+    message: ["hola que tal, como estas?"],
+    photo: "src='prueba/proyecto/img/katy.jpg' alt='Katy'"
+   
+  },
+  {
+    name: "Mariana",
+    message: ["hola que tal, como estas?"],
+    photo: "src='prueba/proyecto/img/mariana.jpg' alt='Mariana'"
+   
+  },
+  {
+    name: "Maria Paula",
+    message: ["hola que tal, como estas?"],
+    photo: "src='prueba/proyecto/img/mariapaula.jpg' alt='Maria Paula'"
+   
+  },
+  {
+    name: "Raymi",
+    message: ["hola que tal, como estas?"],
+    photo: "src='prueba/proyecto/img/raymi.jpg' alt='Raymi'"
+   
+  }
+
+];
